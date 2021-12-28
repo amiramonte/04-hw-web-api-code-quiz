ebfeec3b83caa01a796e957ca27d7955a0195aae
Alex Miramontes <alex.r.miramontes@gmail.com>
2021-12-28 13:38:45 -0800

    adjusted file structure, added highscores html, added 3 sections in html, added javascript document selectors, added css to header

---
+++ assets/script/script.js
@@ -0,0 +1,14 @@
+// import elements from the dom
+let startQuizZoneEl = document.querySelector("#start-quiz-zone");
+let questionZoneEl = document.querySelector("#question-zone");
+let resultsZoneEl = document.querySelector("#results-zone");
+
+
+
+
+
+
+
+
+questionZoneEl.style.display = "none";
+resultsZoneEl.style.display = "none";
