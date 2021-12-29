95c9d219cd07495eeca9b0b6dea18579dc328ef5
Alex Miramontes <alex.r.miramontes@gmail.com>
2021-12-29 10:49:44 -0800

    added id in html to start quiz button, added element for start quiz button in js, added 2 functions in js, added event listener for start quiz button in js

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,8 +2,22 @@
 let startQuizZoneEl = document.querySelector("#start-quiz-zone");
 let questionZoneEl = document.querySelector("#question-zone");
 let resultsZoneEl = document.querySelector("#results-zone");
+let startQuizButtonEl = document.querySelector("start-quiz");
 
 
+// Functions
+
+function buildQuiz() {
+
+}
+
+function showHighScores() {
+
+}
+
+// Eventlisteners for button clicks
+
+startQuizButtonEl.addEventListener("click", buildQuiz);
 
 
 
