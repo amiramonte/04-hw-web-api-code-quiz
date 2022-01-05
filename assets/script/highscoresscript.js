0ce42f362a2ec58fd833b0cf464cf4b9fe3ef16c
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-01-04 19:15:35 -0800

    added parse and for loop to postScores function in highscores js file, added css elements to index.html and added css file and css elements to highscores.html file

--- assets/script/highscoresscript.js
+++ assets/script/highscoresscript.js
@@ -3,6 +3,8 @@ let resetQuizButton = document.querySelector("#reset-quiz");
 
 let resetHighScoresButton = document.querySelector("#reset-highscores");
 
+let scoreListEl = document.querySelector("#score-list");
+
 // functions
 
 function resetQuiz () {
@@ -11,9 +13,20 @@ function resetQuiz () {
 
 function postScores() {
     // set variable where json.parse local storage
+    let runningScores = JSON.parse(localStorage.getItem("highscores"));
+    
 
     // set for loop using variable declared above
-    // declare html table in html file for score-list
+    for (var i = 0; i < runningScores.length; i++) {
+        let newScore = runningScores[i];
+
+        let td = document.createElement("td");
+        td.textContent = newScore;
+
+        scoreListEl.appendChild(td);
+
+
+    }
     // put initials and scores headers in html table
     // push vaules from json.parse array to appropriate places in html table
 }
